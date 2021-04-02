e4e700ce6a7ec4a01a9f8264b64c4ee0f7fd2c52
Jogius <jogius.julius.makowski@gmail.com>
2021-04-02 23:05:36 +0200

    Added 'add' route

---
+++ src/add.php
@@ -0,0 +1,59 @@
+<?php
+require "./vendor/autoload.php";
+// Require dependencies
+use Dotenv\Dotenv;
+require "./utils/DatabaseConnector.php";
+
+// // Set response headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Read values from .env
+$dotenv = new DotEnv(__DIR__);
+$dotenv->load();
+
+// Initialize Database connection
+$dbConnection = (new DatabaseConnector())->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (
+  empty($data->flowtemp) ||
+  empty($data->refluxtemp) ||
+  empty($data->timestamp)
+)
+{
+  http_response_code(400);
+  echo json_encode(array("message" => "Invalid request data."));
+  return;
+}
+
+try
+{
+  $query = "INSERT INTO data(flowtemp, refluxtemp, timestamp) VALUES(:flowtemp, :refluxtemp, :timestamp);";
+
+  $statement = $dbConnection->prepare($query);
+  $statement->bindParam(":flowtemp", $data->flowtemp);
+  $statement->bindParam(":refluxtemp", $data->refluxtemp);
+  $statement->bindParam(":timestamp", $data->timestamp);
+
+  $success = $statement->execute();
+  
+  if ($success)
+  {
+    http_response_code(201);
+    echo json_encode(array("message" => "Success."));
+  }
+  else
+  {
+    http_response_code(403);
+    echo json_encode(array("message" => "Internal error."));
+  }
+}
+catch (PDOException $e)
+{
+  http_response_code(400);
+  echo json_encode(array("message" => $e->getmessage()));
+}
+
+
