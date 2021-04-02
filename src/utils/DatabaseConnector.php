5de7c56b883de0f07878b4813c8f3c37a932056b
Jogius <jogius.julius.makowski@gmail.com>
2021-04-02 22:54:25 +0200

    Added DatabaseConnector

---
+++ src/utils/DatabaseConnector.php
@@ -0,0 +1,50 @@
+<?php
+use Dotenv\Dotenv;
+
+class DatabaseConnector
+{
+  private $dbConnection = null;
+
+  public function __construct()
+  {
+    $dotenv = new DotEnv(__DIR__ . "/..");
+    $dotenv->load();
+
+    $host = getenv('DB_HOST');
+    $port = getenv('DB_PORT');
+    $db   = getenv('DB_DATABASE');
+    $username = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
+
+    try
+    {
+      $this->dbConnection = new PDO(
+        "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$db",
+          $username,
+          $password
+      );
+      $this->dbConnection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+      $statement = "
+        CREATE TABLE IF NOT EXISTS data (
+          id INT NOT NULL AUTO_INCREMENT,
+          flowtemp DOUBLE(4, 2) NOT NULL,
+          refluxtemp DOUBLE(4, 2) NOT NULL,
+          timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          PRIMARY KEY (id)
+        ) ENGINE=INNODB;
+      ";
+
+      $this->dbConnection->exec($statement);
+    }
+    catch (PDOException $e)
+    {
+      exit($e->getMessage());
+    }
+  }
+
+  public function getConnection()
+  {
+    return $this->dbConnection;
+  }
+}
